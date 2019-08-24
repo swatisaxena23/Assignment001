b973af30c7b698314222a865038c39f1fcc43cf5
swatisaxena <swati.saxena_cs18@gla.ac.in>
2019-08-24 19:49:39 +0530

    initial commit variables declared

---
+++ src/gravity/Gravity.java
@@ -0,0 +1,13 @@
+package gravity;
+
+import java.util.Scanner;
+
+public class Gravity {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double time = sc.nextInt();
+        double grav = 9.8;
+        double speed = 0.0;
+        double distance = 0.0;
+    }
+}
